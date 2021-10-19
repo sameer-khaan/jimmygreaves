ef0baabb72530e7c961e3ca7ac1e4a5052323bd7
Sameer Khan <sameerkhan5130@gmail.com>
2021-10-20 01:12:28 +0500

    update

--- footer.php
+++ footer.php
@@ -36,10 +36,10 @@
         </div>
 
         <div style="display:flex;width:100%;margin-top:30px" class="footer_bottom">
-            <span style="font-size:14px;color:white!important;">Registered Charity No. 1189905.</span>
-            <span style="margin-left:auto;color:white!important;font-size:14px;">
+            <p style="font-size:14px;color:white;">Registered Charity No. <span style="font-size:14px;color:white!important;">1189905</span></p>
+            <p style="margin-left:auto;color:white;font-size:14px;">
                 <a href="http://essexwebdesignstudio.com/" target="_blank" style="color:#fff;text-decoration:underline;">Website by EWDS</a>
-            </span>
+            </p>
         </div>
 
     </div>
